6bf8522d9ff2355ebabd093f94c361bcf8032009
JuanMa23 <juanma.panta@gmail.com>
2021-12-04 22:05:26 -0500

    Operaciones matematicas

---
+++ operaciones.R
@@ -0,0 +1,27 @@
+
+# Operaciones en R
+
+n1 <- 8
+n2 <- 5
+
+# suma de numeros
+n1+n2
+
+# resta de numeros
+n1-n2
+
+# multiplicacion de numeros
+n1*n2
+
+# division de numeros
+n1/n2
+
+# potencia de numeros
+n1^n2
+
+
+
+
+
+
+
